3ca36f34a71a59e0f589d06aa0b9a631f387b8b7
yishai <yishai5555@gmail.com>
2016-08-18 15:56:19 +0300

    b2

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function App() {
+  return React.createElement('div', null,
+      React.createElement    ('h1',null ,'I am a title'),
+      React.createElement    ('h2',null ,'I am a sub title')
+  );
+}
+
+function App1() {
   return React.createElement('div', null, 'I am a component' );
 }
 
