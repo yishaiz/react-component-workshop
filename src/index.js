1e151e437cfdec009b11656e908dc3fe5d0708f8
yishaiz <yishai5555@gmail.com>
2016-08-19 18:28:55 +0300

    b17

--- src/index.js
+++ src/index.js
@@ -4,25 +4,27 @@ import Recipes from './recipes';
 
 let recipes = ["Waffel", "Omellete", "use state"];
 
-
 class App extends React.Component{
     constructor(){
         super();
 
         this.state = {recipes};
+
+        setTimeout(() => {
+            // recipes.push('Shakshuka')
+            recipes: this.state.recipes.concat('Shakshuka')
+            // alert(111);
+        },1500);
     }
 
     render(){
         return(
             <div>
-                {/*<Recipes recipes={recipes}/>*/}
                 <Recipes recipes={this.state.recipes}/>
             </div>
         )
     }
 }
-
-
 
 ReactDOM.render(
     React.createElement(App),
