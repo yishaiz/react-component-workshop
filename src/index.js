7995e9081789e50324516da976148c9cd33d5a69
yishaiz <yishai5555@gmail.com>
2016-08-19 15:59:02 +0300

    b3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
+
 function App() {
+    return(
+        <div>
+            <h1>I am a title</h1>
+            <h2>I am a subtitle</h2>
+        </div>
+    );
+}
+
+
+function App2() {
   return React.createElement('div', null,
       React.createElement    ('h1',null ,'I am a title'),
       React.createElement    ('h2',null ,'I am a sub title')
