180a317df45b4cc2579bc65d3c3537211435688c
yishaiz <yishai5555@gmail.com>
2016-08-19 16:19:30 +0300

    b4

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from "react-dom";
 
 
 
-function App() {
+let App = () => {
+    return(
+        <div>
+            <h1>I am a title</h1>
+            <h2>I am a subtitle</h2>
+        </div>
+    );
+}
+/*
+function App3() {
     return(
         <div>
             <h1>I am a title</h1>
@@ -23,6 +32,9 @@ function App2() {
 function App1() {
   return React.createElement('div', null, 'I am a component' );
 }
+
+*/
+
 
 ReactDOM.render(
     React.createElement(App),
