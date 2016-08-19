bbecdbeb3abcfa6fd81bb11d65b17e918dff79c6
yishaiz <yishai5555@gmail.com>
2016-08-19 16:31:15 +0300

    b5

--- src/index.js
+++ src/index.js
@@ -3,38 +3,13 @@ import ReactDOM from "react-dom";
 
 
 
-let App = () => {
-    return(
+const App = () =>
+    (
         <div>
             <h1>I am a title</h1>
             <h2>I am a subtitle</h2>
         </div>
     );
-}
-/*
-function App3() {
-    return(
-        <div>
-            <h1>I am a title</h1>
-            <h2>I am a subtitle</h2>
-        </div>
-    );
-}
-
-
-function App2() {
-  return React.createElement('div', null,
-      React.createElement    ('h1',null ,'I am a title'),
-      React.createElement    ('h2',null ,'I am a sub title')
-  );
-}
-
-function App1() {
-  return React.createElement('div', null, 'I am a component' );
-}
-
-*/
-
 
 ReactDOM.render(
     React.createElement(App),
