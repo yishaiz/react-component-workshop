3f5c92414e9407989a4c53606625ca3d73f205fb
yishaiz <yishai5555@gmail.com>
2016-08-21 22:22:40 +0300

    b18

--- src/index.js
+++ src/index.js
@@ -19,25 +19,47 @@ class App extends React.Component {
         setTimeout(
             ()=> {
                 // recipes.push("Shakshuka")
-                this.setState ({
+                this.setState({
                     recipes: this.state.recipes.concat("Shakshuka")
                 })
             }, 1500
         );
 
+    }
 
+
+    addRecipe(event) {
+
+        event.preventDefault();
+
+        const value = this.refs.recipeNameInput.value;
+
+        console.log('new value', value);
+
+        this.setState({
+            recipes: this.state.recipes.concat(value)
+        });
+
+        this.refs.recipeNameInput.value = '';
     }
 
     render() {
         return (
             <div>
                 <Recipes recipes={this.state.recipes}/>
+
+                <form onSubmit={this.addRecipe.bind(this)}>
+                    <input type="text" ref="recipeNameInput"/>
+                    <button>Add Recipe</button>
+                </form>
+
             </div>
         )
     }
 }
 
 ReactDOM.render(
-    React.createElement(App),
+    // React.createElement(App),
+    <App/>,
     document.getElementById('root')
 );
