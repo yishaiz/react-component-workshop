93753d2fc04361778f9a0f52f60e169bb1a86953
yishaiz <yishai5555@gmail.com>
2016-08-26 14:22:52 +0300

    b22

--- src/recipe.js
+++ src/recipe.js
@@ -6,11 +6,7 @@
  );
 
  export default Recipe;
-*/
-
-
-
-
+ */
 
 
 import React from "react";
@@ -20,20 +16,37 @@ class Recipe extends React.Component {
     constructor(props) {
         super();
 
-        console.log('constructor' ,props);
+        console.log('constructor', props);
     }
 
     render() {
-        console.log('render',this.props);
+        console.log('render', this.props);
 
         return (
             <li>{this.props.name}</li>
         )
     }
 
+    componentWillMount() {
+        console.log('I will mount - Recipe')
+    }
+
     componentDidMount() {
         console.log('I am mounted ! - Recipe')
     }
+
+    componentWillUnmount() {
+        console.log('I am unmounted !')
+    }
+
+
+    shouldComponentUpdate(newProps, newState) {
+        // return false;
+        console.log('should component update',this.props == newProps);
+
+        return this.props == newProps;
+    }
+
 }
 
 export default Recipe;
